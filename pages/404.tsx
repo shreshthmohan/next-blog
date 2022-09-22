91ab0117089001b5db60d87d26cd84aef0462c4d
Shreshth Mohan <sremog@gmail.com>
2022-09-22 17:23:59 +0530

    add missing 404 file (#58)

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import BaseReadingLayout from 'layouts/BaseReadingLayout'
+
+export default function Custom404() {
+  return (
+    <BaseReadingLayout title="404 - Not found">
+      <h1>404, page not found.</h1>
+      <p>Looks like you took a wrong turn. :)</p>
+    </BaseReadingLayout>
+  )
+}
