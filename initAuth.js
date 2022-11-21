fdfb0f05a3be0e049931c9888ab2f7ce263aabfa
Shreshth Mohan <sremog@gmail.com>
2022-11-21 17:42:26 +0530

    auth is working

--- initAuth.js
+++ initAuth.js
@@ -1,58 +1,98 @@
-// ./initAuth.js
+/* globals window */
 import { init } from 'next-firebase-auth'
+import absoluteUrl from 'next-absolute-url'
+
+const TWELVE_DAYS_IN_MS = 12 * 60 * 60 * 24 * 1000
 
 const initAuth = () => {
   init({
-    authPageURL: '/auth',
-    appPageURL: '/',
-    loginAPIEndpoint: '/api/login', // required
-    logoutAPIEndpoint: '/api/logout', // required
-    onLoginRequestError: err => {
-      console.error(err)
+    // debug: true,
+
+    // This demonstrates setting a dynamic destination URL when
+    // redirecting from app pages. Alternatively, you can simply
+    // specify `authPageURL: '/auth-ssr'`.
+    authPageURL: ({ ctx }) => {
+      const isServerSide = typeof window === 'undefined'
+      const origin = isServerSide
+        ? absoluteUrl(ctx.req).origin
+        : window.location.origin
+      const destPath =
+        typeof window === 'undefined' ? ctx.resolvedUrl : window.location.href
+      const destURL = new URL(destPath, origin)
+      return `auth-ssr?destination=${encodeURIComponent(destURL)}`
     },
-    onLogoutRequestError: err => {
-      console.error(err)
+
+    // This demonstrates setting a dynamic destination URL when
+    // redirecting from auth pages. Alternatively, you can simply
+    // specify `appPageURL: '/'`.
+    appPageURL: ({ ctx }) => {
+      const isServerSide = typeof window === 'undefined'
+      const origin = isServerSide
+        ? absoluteUrl(ctx.req).origin
+        : window.location.origin
+      const params = isServerSide
+        ? new URL(ctx.req.url, origin).searchParams
+        : new URLSearchParams(window.location.search)
+      const destinationParamVal = params.get('destination')
+        ? decodeURIComponent(params.get('destination'))
+        : undefined
+
+      // By default, go to the index page if the destination URL
+      // is invalid or unspecified.
+      let destURL = '/'
+      if (destinationParamVal) {
+        // Verify the redirect URL host is allowed.
+        // https://owasp.org/www-project-web-security-testing-guide/v41/4-Web_Application_Security_Testing/11-Client_Side_Testing/04-Testing_for_Client_Side_URL_Redirect
+        const allowedHosts = ['localhost:3000']
+        const allowed =
+          allowedHosts.indexOf(new URL(destinationParamVal).host) > -1
+        if (allowed) {
+          destURL = destinationParamVal
+        } else {
+          // eslint-disable-next-line no-console
+          console.warn(
+            `Redirect destination host must be one of ${allowedHosts.join(
+              ', ',
+            )}.`,
+          )
+        }
+      }
+      return destURL
     },
-    // firebaseAuthEmulatorHost: 'localhost:9099',
+    loginAPIEndpoint: '/api/login',
+    logoutAPIEndpoint: '/api/logout',
     firebaseAdminInitConfig: {
       credential: {
-        projectId: process.env.FIREBASE_PROJECT_ID,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // The private key must not be accessible on the client side.
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        // Using JSON to handle newline problems when storing the
+        // key as a secret in Vercel. See:
+        // https://github.com/vercel/vercel/issues/749#issuecomment-707515089
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+          ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+          : undefined,
       },
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     },
-    // Use application default credentials (takes precedence over firebaseAdminInitConfig if set)
-    // useFirebaseAdminDefaultCredential: true,
-
     firebaseClientInitConfig: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     },
     cookies: {
-      name: 'NextBlog', // required
-      // Keys are required unless you set `signed` to `false`.
-      // The keys cannot be accessible on the client side.
+      name: 'NextBlog',
       keys: [
-        process.env.FIREBASE_COOKIE_SECRET_CURRENT,
-        process.env.FIREBASE_COOKIE_SECRET_PREVIOUS,
+        process.env.COOKIE_SECRET_CURRENT,
+        process.env.COOKIE_SECRET_PREVIOUS,
       ],
       httpOnly: true,
-      maxAge: 12 * 60 * 60 * 24 * 1000, // twelve days
+      maxAge: TWELVE_DAYS_IN_MS,
       overwrite: true,
       path: '/',
       sameSite: 'strict',
-      secure: true, // set this to false in local (non-HTTPS) development
+      secure: process.env.NEXT_PUBLIC_COOKIE_SECURE === 'true',
       signed: true,
-    },
-    onVerifyTokenError: err => {
-      console.error(err)
-    },
-    onTokenRefreshError: err => {
-      console.error(err)
     },
   })
 }
--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,6 +1,6 @@
 // ./pages/api/login
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { setAuthCookies } from 'next-firebase-auth'
+import { unsetAuthCookies } from 'next-firebase-auth'
 import initAuth from '../../initAuth' // the module you created above
 import type { Data } from './login'
 
@@ -8,9 +8,11 @@ initAuth()
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
-    await setAuthCookies(req, res)
+    await unsetAuthCookies(req, res)
   } catch (e) {
-    return res.status(500).json({ error: 'Unexpected error.' })
+    return res
+      .status(500)
+      .json({ error: `Unexpected error: ${JSON.stringify(e)}` })
   }
   return res.status(200).json({ success: true })
 }
