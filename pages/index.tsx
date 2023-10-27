d10d96f06f19332de544fda652e9a90cd5d68b7a
Shreshth Mohan <sremog@gmail.com>
2023-10-27 19:36:13 +0530

    styles; map simiar links

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const DataVizCard = ({
   imgAlt = title,
   hrefPath,
 }) => (
-  <div className="mb-4 rounded border border-solid border-gray-300 px-3 py-2 shadow-sm">
+  <div className="mb-4 rounded border border-solid border-gray-300 px-3 py-2 shadow-sm dark:border-gray-600">
     <h3 className="m-0 text-xl font-normal">{title}</h3>
     <p className="m-0 text-base leading-tight text-gray-500">{description}</p>
     <a
@@ -28,6 +28,32 @@ const DataVizCard = ({
     </a>
   </div>
 )
+
+const thingsIHaveMade = [
+  {
+    title: 'Bulls and Cows: A guessing game',
+    description:
+      'A guessing game where you have to guess a number with no repeated digits. The game gives you hints in the form of bulls and cows. A bull means a digit is in the right place, a cow means a digit is in the wrong place. For example, if the secret number is 1234 and you guess 1359, the game will give you 1 bull and 1 cow.',
+    hrefPath: '/bullsandcows',
+  },
+  {
+    title: 'Find Nearest Tailwind CSS color tool',
+    description:
+      'A tool to find the nearest Tailwind CSS color to a given color. It uses the CIEDE2000 algorithm to calculate the distance between colors.',
+    external: true,
+    hrefPath: 'https://nearest-tailwind-color.netlify.app/',
+  },
+  {
+    title: 'curved-polygon npm package',
+    hrefPath: 'https://www.npmjs.com/package/curved-polygon',
+    external: true,
+  },
+  {
+    title: 'Blank new page Chrome extension',
+    hrefPath: 'https://github.com/shreshthmohan/blank-new-tab',
+    external: true,
+  },
+]
 
 const Home: NextPage = () => {
   return (
@@ -84,7 +110,7 @@ const Home: NextPage = () => {
         imgWidth="851"
       />
 
-      <div className="mb-4 rounded border border-solid border-gray-300 px-3 py-2 shadow-sm">
+      <div className="mb-4 rounded border border-solid border-gray-300 px-3 py-2 shadow-sm dark:border-gray-600">
         <p className="m-0 text-xl leading-tight">
           See all the{' '}
           <a
@@ -97,58 +123,89 @@ const Home: NextPage = () => {
         </p>
       </div>
 
-      <h2 className="mb-4 pt-12 text-2xl font-normal">Tiny projects</h2>
-      <div className="rounded border border-solid border-gray-300 p-3">
-        <h3 className="m-0 text-xl font-normal">
-          <Link href="/curvedcrop">
-            <a>Cropping tool for Twitter</a>
-          </Link>
-        </h3>
-        <p className="m-0 text-base leading-tight text-gray-500">
-          Create polygon-shaped crops, with curved corners and some fun patterns
-          created due to corner cases in my polygon curve code. Some images I
-          cropped using the tool:
-        </p>
-        <div className="mt-2 flex flex-wrap justify-center rounded border border-solid border-gray-100 bg-gray-200 p-2">
-          <Link href="/curvedcrop?sides=26&border-radius=1396&circum-radius=145">
-            <a>
-              <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop2.png" />
-            </a>
-          </Link>
-          <Link href="/curvedcrop?sides=10&border-radius=1070&circum-radius=181">
-            <a>
-              <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop10.png" />
-            </a>
-          </Link>
-          <Link href="/curvedcrop?circum-radius=201&sides=8&border-radius=490">
-            <a>
-              <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop6.png" />
-            </a>
-          </Link>
-          <Link href="/curvedcrop?circum-radius=206&sides=6&border-radius=50">
-            <a>
-              <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop4.png" />
-            </a>
-          </Link>
-          <Link href="/curvedcrop?sides=3&border-radius=0&rotate=90">
-            <a>
-              <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop11.png" />
-            </a>
-          </Link>
-          <Link href="/curvedcrop?sides=7&border-radius=50&circum-radius=204">
-            <a>
-              <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop5.png" />
-            </a>
-          </Link>
+      <h2 className="mb-4 pt-12 text-2xl font-normal">
+        Other things I have made
+      </h2>
+      <div className="flex flex-col gap-y-4">
+        <div className="rounded border border-solid border-gray-300 p-3 dark:border-gray-600">
+          <h3 className="m-0 text-xl font-normal">
+            <Link href="/curvedcrop">
+              <a>Cropping tool for Twitter</a>
+            </Link>
+          </h3>
+          <p className="m-0 text-base leading-tight text-gray-500">
+            Create polygon-shaped crops, with curved corners and some fun
+            patterns created due to corner cases in my polygon curve code. Some
+            images I cropped using the tool:
+          </p>
+          <div className="mt-2 flex flex-wrap justify-center rounded border border-solid border-gray-100 bg-gray-200 p-2">
+            <Link href="/curvedcrop?sides=26&border-radius=1396&circum-radius=145">
+              <a>
+                <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop2.png" />
+              </a>
+            </Link>
+            <Link href="/curvedcrop?sides=10&border-radius=1070&circum-radius=181">
+              <a>
+                <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop10.png" />
+              </a>
+            </Link>
+            <Link href="/curvedcrop?circum-radius=201&sides=8&border-radius=490">
+              <a>
+                <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop6.png" />
+              </a>
+            </Link>
+            <Link href="/curvedcrop?circum-radius=206&sides=6&border-radius=50">
+              <a>
+                <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop4.png" />
+              </a>
+            </Link>
+            <Link href="/curvedcrop?sides=3&border-radius=0&rotate=90">
+              <a>
+                <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop11.png" />
+              </a>
+            </Link>
+            <Link href="/curvedcrop?sides=7&border-radius=50&circum-radius=204">
+              <a>
+                <CroppedImageSample imgSrc="/images/ccdemo/curvedcrop5.png" />
+              </a>
+            </Link>
+          </div>
         </div>
-      </div>
 
-      <h3 className=" text-xl font-normal">
-        <Link href="/bullsandcows">
-          <a>Bulls and Cows: A guessing game</a>
-        </Link>
-      </h3>
-      <br />
+        {thingsIHaveMade.map(thing => {
+          if (thing.external) {
+            return (
+              <div
+                key={thing.title}
+                className="rounded border border-solid border-gray-300 px-3 py-2 shadow-sm dark:border-gray-600"
+              >
+                <h3 className="m-0 text-xl font-normal">
+                  <a
+                    target="_blank"
+                    href={thing.hrefPath}
+                    rel="noopener noreferrer"
+                  >
+                    {thing.title}
+                  </a>
+                </h3>
+              </div>
+            )
+          } else {
+            return (
+              <div
+                key={thing.title}
+                className="rounded border border-solid border-gray-300 px-3 py-2 shadow-sm dark:border-gray-600"
+              >
+                <h3 className="m-0 text-xl font-normal">
+                  <Link href={thing.hrefPath}>
+                    <a>{thing.title}</a>
+                  </Link>
+                </h3>
+              </div>
+            )
+          }
+        })}
+      </div>
     </BaseReadingLayout>
   )
 }
